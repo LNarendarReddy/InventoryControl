974a5432dd482a88b6a9a477b7bb3e791e5a7f23
sainathgoud504 <31504412+sainathgoud504@users.noreply.github.com>
2021-12-12 12:25:17 +0530

    service changes with logging

--- Services/Warehouse/WarehouseCloudSync/WarehouseCloudSync/SyncData.cs
+++ Services/Warehouse/WarehouseCloudSync/WarehouseCloudSync/SyncData.cs
@@ -8,17 +8,24 @@ namespace WarehouseCloudSync
     {
         public void StartSync()
         {
+            LoggerUtility.InitializeLogger();
             DateTime syncStartTime = DateTime.Now;
+            LoggerUtility.Logger.Info($"Warehouse sync started at {syncStartTime.ToLongTimeString()}");
             WarehouseRepository warehouseRepository = new WarehouseRepository();
             CloudRepository cloudRepository = new CloudRepository();
             DataTable dtEntity = warehouseRepository.GetEntityWiseData("ENTITY", 11);
             foreach (DataRow entityRow in dtEntity.Rows)
             {
                 string entityName = entityRow["ENTITYNAME"].ToString();
+                LoggerUtility.Logger.Info($"{entityName} sync started");
                 DataTable dtEntityWiseData = warehouseRepository.GetEntityWiseData(entityName, 11);
+                LoggerUtility.Logger.Info($"Found {dtEntityWiseData.Rows.Count} records to sync in entity : {entityName} ");
                 cloudRepository.SaveData(entityName, dtEntityWiseData);
                 warehouseRepository.UpdateEntitySyncStatus(entityName, 11, syncStartTime);
+                LoggerUtility.Logger.Info($"{entityName} sync completed");
             }
+
+            LoggerUtility.Logger.Info($"Warehouse sync completed");
         }
     }
 }
