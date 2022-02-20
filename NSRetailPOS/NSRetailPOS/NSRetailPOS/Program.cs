3869bacebcd83a03630c30654adbb67b74d106be
Sainath Goud Ambala <sainathgoud504@gmail.com>
2022-02-20 13:47:02 +0530

    grant access to directory

--- NSRetailPOS/NSRetailPOS/NSRetailPOS/Program.cs
+++ NSRetailPOS/NSRetailPOS/NSRetailPOS/Program.cs
@@ -8,8 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -33,14 +36,32 @@ namespace NSRetailPOS
             SkinManager.EnableFormSkins();
             SkinManager.EnableMdiFormSkins();
             DataTable dt = new POSRepository().GetPOSConfiguration();
-            if(dt != null && dt.Rows.Count > 0)
+            if (dt != null && dt.Rows.Count > 0)
             {
                 Utility.branchinfo.BranchID = dt.Rows[0]["BRANCHID"];
                 Utility.branchinfo.BranchCounterID = dt.Rows[0]["BRANCHCOUNTERID"];
                 Application.Run(new frmLogin());
             }
             else
+            {
+                GrantAccess();
                 Application.Run(new frmConfiguration());
+            }
+        }
+
+        private static void GrantAccess()
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(Environment.CurrentDirectory);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule(
+                new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                FileSystemRights.FullControl,
+                InheritanceFlags.ObjectInherit |
+                   InheritanceFlags.ContainerInherit,
+                PropagationFlags.NoPropagateInherit,
+                AccessControlType.Allow));
+
+            dInfo.SetAccessControl(dSecurity);
         }
     }
     public class SkinRegistration : Component
