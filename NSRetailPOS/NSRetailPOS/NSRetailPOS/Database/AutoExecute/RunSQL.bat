27215432a0afa66a0f244d1a243fa361ed1066d2
sainathgoud504 <31504412+sainathgoud504@users.noreply.github.com>
2022-06-24 21:23:45 +0530

    Changed server names and fixed billnum jump issue in POS

--- NSRetailPOS/NSRetailPOS/NSRetailPOS/Database/AutoExecute/RunSQL.bat
+++ NSRetailPOS/NSRetailPOS/NSRetailPOS/Database/AutoExecute/RunSQL.bat
@@ -1,6 +1,8 @@
 @echo off
-@REM sqlcmd -S .\SQLEXPRESS -i POS_update.sql
-sqlcmd -S (LocalDB)\MSSQLLocalDB -i POS_update.sql
-
-echo script complete
+@echo executing POS script
+sqlcmd -S .\SQLEXPRESS -i POS_update.sql
+rem sqlcmd -S (LocalDB)\MSSQLLocalDB -i POS_update.sql
+@echo script complete
 pause
+
+
