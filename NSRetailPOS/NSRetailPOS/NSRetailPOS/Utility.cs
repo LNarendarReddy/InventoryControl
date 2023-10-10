ae3b4fa038c5117a6d945142c295486b94da47ec
LNarendarReddy <narendar.reddy@betasystems.com>
2023-10-10 21:54:29 +0530

    Changes for MRP selection when no stock is available
    Changes for MRP selection when no stock is available
    Offer issue fixed
    Version changes

--- NSRetailPOS/NSRetailPOS/NSRetailPOS/Utility.cs
+++ NSRetailPOS/NSRetailPOS/NSRetailPOS/Utility.cs
@@ -313,9 +313,9 @@ namespace NSRetailPOS
             Form.ActiveForm.BeginInvoke((Action)(() => (Form.ActiveForm as IBarcodeReceiver).ReceiveBarCode(data)));
         }
 
-        public static string AppVersion = "1.3.9";
+        public static string AppVersion = "1.4.0";
         public static string DBVersion = string.Empty;
-        public static string VersionDate = "(09-10-2023)";
+        public static string VersionDate = "(10-10-2023)";
 
         private static bool DBVersionCheck(BackgroundWorker backgroundWorker, CloudRepository cloudRepository, SyncRepository syncRepository)
         {
