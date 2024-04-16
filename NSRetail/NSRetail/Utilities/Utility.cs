777c0ffffbf6a430f3e9611fefb49846d3901fc2
sainathgoud504 <sainathgoud504@gmail.com>
2024-04-16 13:53:14 +0530

    minor issue fix and version bump

--- NSRetail/NSRetail/Utilities/Utility.cs
+++ NSRetail/NSRetail/Utilities/Utility.cs
@@ -51,8 +51,8 @@ namespace NSRetail
         public static string BarcodePrinter = string.Empty;
         public static string A4SizePrinter = string.Empty;
         public static string ThermalPrinter = string.Empty;
-        public static string AppVersion = "2.3.6";
-        public static string VersionDate = "(15-04-2024)";
+        public static string AppVersion = "2.3.7";
+        public static string VersionDate = "(16-04-2024)";
 
         public static void Setfocus(GridView view, string ColumnName, object Value)
         {
@@ -214,6 +214,7 @@ namespace NSRetail
             dtBranchList = null;
             dtCatgeory = null;
             dtSubCatgeory = null;
+            dtBranchListIncludingDeleted = null;
 
             FillItemBaseline();
             GetGSTBaseline();
