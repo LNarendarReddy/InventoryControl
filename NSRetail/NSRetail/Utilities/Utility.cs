9dbb7c689fedceb21844cb6ae74bf110434846ab
LNarendarReddy <nandu9605@gmail.com>
2025-03-13 10:19:44 +0530

    Version info

--- NSRetail/NSRetail/Utilities/Utility.cs
+++ NSRetail/NSRetail/Utilities/Utility.cs
@@ -54,8 +54,8 @@ namespace NSRetail
         public static string BarcodePrinter = string.Empty;
         public static string A4SizePrinter = string.Empty;
         public static string ThermalPrinter = string.Empty;
-        public static string AppVersion = "2.9.3";
-        public static string VersionDate = "(03-03-2025)";
+        public static string AppVersion = "2.9.4";
+        public static string VersionDate = "(13-03-2025)";
 
         public static void Setfocus(GridView view, string ColumnName, object Value)
         {
--- Setup/NSRetail/NSRetail.iss
+++ Setup/NSRetail/NSRetail.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "NSRetail"
-#define MyAppVersion "2.9.3(03-03-2025)"
+#define MyAppVersion "2.9.4(13-03-2025)"
 #define MyAppPublisher "N SOFT SOLUTIONS"
 #define MyAppURL "http://www.nsoftsol.com/"
 #define MyAppExeName "NSRetail.exe"
