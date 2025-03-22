30f80bf5bb20bf745195b63c62eaaed17d7be427
LNarendarReddy <nandu9605@gmail.com>
2025-03-22 22:12:32 +0530

    Version info

--- NSRetail/NSRetail/Utilities/Utility.cs
+++ NSRetail/NSRetail/Utilities/Utility.cs
@@ -54,8 +54,8 @@ namespace NSRetail
         public static string BarcodePrinter = string.Empty;
         public static string A4SizePrinter = string.Empty;
         public static string ThermalPrinter = string.Empty;
-        public static string AppVersion = "2.9.8";
-        public static string VersionDate = "(20-03-2025)";
+        public static string AppVersion = "2.9.9";
+        public static string VersionDate = "(22-03-2025)";
 
         public static void Setfocus(GridView view, string ColumnName, object Value)
         {
