a27ccede6cd4d1687e73869e2fd91953b4d5efc8
LNarendarReddy <nandu9605@gmail.com>
2025-04-05 09:14:34 +0530

    Changes for dispatch revert

--- NSRetail/NSRetail/Utilities/Utility.cs
+++ NSRetail/NSRetail/Utilities/Utility.cs
@@ -54,7 +54,7 @@ namespace NSRetail
         public static string BarcodePrinter = string.Empty;
         public static string A4SizePrinter = string.Empty;
         public static string ThermalPrinter = string.Empty;
-        public static string AppVersion = "3.0.3";
+        public static string AppVersion = "3.0.4";
         public static string VersionDate = "(05-04-2025)";
 
         public static void Setfocus(GridView view, string ColumnName, object Value)
